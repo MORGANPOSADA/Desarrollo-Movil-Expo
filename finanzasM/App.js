1de5a8df4811cc30cf113a2ea3a9da869c399728
MORGANPOSADA <105472247+MORGANPOSADA@users.noreply.github.com>
2023-02-08 22:53:07 -0600

    Add files via upload

---
+++ finanzasM/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Button } from '@rneui/themed';
+import { initializeApp } from "firebase/app";
+import Navigation from './config/navigation/Navigation';
+
+export default function App() {
+  return (
+    <Navigation/>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ finanzasM/config/stack/ProfileStack.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import Profile from '../../modules/profile/adapters/screens/Profile';
+import UserGuest from '../../modules/profile/adapters/screens/UserGuest';
+import Login from '../../modules/auth/adapters/screens/Login';
+
+const Stack = createNativeStackNavigator();
+export default function ProfileStack() {
+  return (
+    <Stack.Navigator
+        initialRouteName='userGuestStack'
+    screenOptions={{
+        headerMode: 'screen',
+        headerTintColor: 'white',
+        headerStyle: {backgroundColor: '#ff5a60'}
+    }}
+>
+    <Stack.Screen
+        name='profileStack'
+        options={{title: 'Perfil'}}
+        component = {Profile}
+    />
+     <Stack.Screen
+        name='userGuestStack'
+        options={{title: 'Bienvenido'}}
+        component = {UserGuest}
+    />
+    <Stack.Screen
+        name='loginStack'
+        options={{title: 'Inicio de Sesión'}}
+        component = {Login}
+    />
+</Stack.Navigator>
+  )
+}
